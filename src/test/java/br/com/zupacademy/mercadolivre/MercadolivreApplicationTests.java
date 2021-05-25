30fdf8c0fafe02b77586b8c6cebe2782d2b67e90
William Bohn <williamxavero@gmail.com>
2021-05-25 17:18:06 -0300

    setup

---
+++ src/test/java/br/com/zupacademy/mercadolivre/MercadolivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.mercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadolivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
